caa316f3db6a98c0967e8dd456f3fc6d8b4c95e7
saurav darji <sauravdarji8793@gmail.com>
2022-04-06 16:20:36 +0530

    mainscreen

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,4 +1,4 @@
-
+import {useState} from "react";
 import Navbar from "./components/Navbar.js";
 import Footer from "./components/Footer.js";
 import {Routes,Route} from 'react-router-dom';
@@ -6,9 +6,11 @@ import Home from "./components/Home.js";
 import Rules from "./components/Rules.js";
 import About from "./components/About.js";
 import Auth from "./components/Auth.js";
+import StartScreen from "./components/StartScreen.js";
+import Mainscreen from "./components/Mainscreen.js";
 
 function App(){
-
+  const [isQuizStarted,setisQuizStarted] = useState(false);
 
   return(
 
@@ -28,7 +30,17 @@ function App(){
     
     
       <Navbar/>
-      
+ 
+      <div className="Quiz-container">
+        {
+          isQuizStarted ? (
+            <Mainscreen retry ={()=> setisQuizStarted(false)}/>
+          ): (
+            <StartScreen start  ={()=> setisQuizStarted(true)}/>
+          )
+        }
+        
+      </div>
 
 
       <Footer />
---
+++ quiz-app/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+
+
+function StartScreen({start}){
+     return (
+          <div className="Start-Screen">
+               {/* <h2>Start Quiz</h2>
+               <p className="title">INDIA MOST POPULAR COMEDY TELEVSION SHOW.</p> */}
+               <button className= "btn btn1" onClick={start}>GO ON &#8594;</button>
+          </div>
+     );
+};
+
+export default StartScreen;
